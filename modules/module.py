e931c9464868143cf4bbbc3d832cb66785f587ed
Sean Chow <Sean@Seans-MacBook-Pro-3.local>
2016-09-21 14:43:26 -0400

    menu
    few functions
    template

---
+++ modules/module.py
@@ -0,0 +1,22 @@
+def printMenu():
+	print ("1 - Addition 2 - Subtraction 3 - Division")
+	print ("4 - Multiplication 5 - Division")
+	
+
+def addition():
+	print ("Enter two numbers to perform addition.")
+
+def subtraction():
+	print ("Enter two numbers to perform subtraction")
+
+def multiplication():
+	print ("Enter two numbers to perform subtraction")
+
+def division():
+	print ("Enter two numbers to perform division")
+
+def squareRoot():
+	print ("Enter numbers to perform square root")
+
+def squareRoot():
+	print ("Enter numbers to perform square root")
