3c4236d36820e643327a33810f4f1e29a8d1110d
Sean Chow <Sean@Seans-MacBook-Pro-3.local>
2016-11-18 15:34:51 -0500

    added text interface
    not looping

--- src/graphingCalculator.py
+++ src/graphingCalculator.py
@@ -1,21 +1,24 @@
 import GUI
 import sys
 import fileCalc
-
+import textInterface
 # driver file for graphing Calculator
-# takes in command line args to start respective program (1,2,3) , (gui,text line, text file)
-def printMenu(argv,textFile):
+# takes in command line args to start respective program (1,2,3) ,
+# (gui,text line, text file)
+
+
+def printMenu(argv, textFile):
     if argv == "1":
         print "GUI Interface Started!"
         GUI.startGUI()
     elif argv == "2":
         print "Text Line Interface Started!"
-
+        textInterface.startText()
     elif argv == "3":
         print "Text File Interface Started!"
         fileCalc.fileRead(textFile)
 
 if (len(sys.argv) > 2):
-    printMenu(sys.argv[1],sys.argv[2])
+    printMenu(sys.argv[1], sys.argv[2])
 else:
-    printMenu(sys.argv[1],"NULL")
+    printMenu(sys.argv[1], "NULL")
