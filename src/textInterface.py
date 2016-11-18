eb5ee26277416fea8558a9de29a5b1b90dc0fc1c
Sean Chow <Sean@Seans-MacBook-Pro-3.local>
2016-11-18 15:56:11 -0500

    fix bugs
    still opening gui (not working)

--- src/textInterface.py
+++ src/textInterface.py
@@ -1,13 +1,9 @@
-# coding=utf-8
-from Tkinter import *
-import numpy as np
-import random
-import math
-import parser
-import solve
+import os.path
 from pyparsing import Literal, CaselessLiteral, Word, Combine, Group, Optional,\
     ZeroOrMore, Forward, nums, alphas
+import math
 import operator
+import numpy as np
 
 # global stack
 emptyStack = []
@@ -19,38 +15,43 @@ def startText():
 
         path = raw_input("Enter the type of operation you want to perform.\n"
                          "1 - Calculate solution\n"
-                         "2 - Graph Equation\n")
+                         "2 - Graph Equation\n"
+                         "q - Quit\n")
+        global emptyStack
         if (path == "1"):
             equation = raw_input("Enter the equation:\n")
-            global emptyStack
+
             emptyStack = []
             ans = BNF().parseString(equation)
             ans = evaluateStack(emptyStack[:])
-            print str(ans)
-        else:
-            equation = raw_input("Enter the equation X:\n")
+            print 'Answer:',str(ans)
+        elif (path == "2"):
+            eqn = raw_input("Enter the equation X:\n")
             minX = raw_input("Enter X1 (min range):\n")
             maxX = raw_input("Enter X2 (max range):\n")
             deltaX = raw_input("Enter the delta X (step):\n")
-            global emptyStack
             for x in np.arange(float(minX), float(maxX), float(deltaX)):  # x1,x2,step
-                equation = equation.replace("x", str(x))
-                global emptyStack
+                equation = eqn.replace("x", str(x))
+
                 emptyStack = []
                 ans = BNF().parseString(equation)
                 ans = evaluateStack(emptyStack[:])
-                print '(',x,',',ans,')'
+                print '(', x, ',', ans, ')'
 
 
-def pushFirst( strg, loc, toks ):
-    emptyStack.append( toks[0] )
-def pushUMinus( strg, loc, toks ):
-    if toks and toks[0]=='-':
-        emptyStack.append( 'unary -' )
+def pushFirst(strg, loc, toks):
+    emptyStack.append(toks[0])
+
+
+def pushUMinus(strg, loc, toks):
+    if toks and toks[0] == '-':
+        emptyStack.append('unary -')
         #~ exprStack.append( '-1' )
         #~ exprStack.append( '*' )
 
 bnf = None
+
+
 def BNF():
     """
     expop   :: '^'
@@ -64,66 +65,69 @@ def BNF():
     """
     global bnf
     if not bnf:
-        point = Literal( "." )
-        e     = CaselessLiteral( "E" )
-        fnumber = Combine( Word( "+-"+nums, nums ) +
-                           Optional( point + Optional( Word( nums ) ) ) +
-                           Optional( e + Word( "+-"+nums, nums ) ) )
-        ident = Word(alphas, alphas+nums+"_$")
+        point = Literal(".")
+        e = CaselessLiteral("E")
+        fnumber = Combine(Word("+-" + nums, nums) +
+                          Optional(point + Optional(Word(nums))) +
+                          Optional(e + Word("+-" + nums, nums)))
+        ident = Word(alphas, alphas + nums + "_$")
 
-        plus  = Literal( "+" )
-        minus = Literal( "-" )
-        mult  = Literal( "*" )
-        div   = Literal( "/" )
-        lpar  = Literal( "(" ).suppress()
-        rpar  = Literal( ")" ).suppress()
-        addop  = plus | minus
+        plus = Literal("+")
+        minus = Literal("-")
+        mult = Literal("*")
+        div = Literal("/")
+        lpar = Literal("(").suppress()
+        rpar = Literal(")").suppress()
+        addop = plus | minus
         multop = mult | div
-        expop = Literal( "^" )
-        pi    = CaselessLiteral( "PI" )
+        expop = Literal("^")
+        pi = CaselessLiteral("PI")
 
         expr = Forward()
-        atom = (Optional("-") + ( pi | e | fnumber | ident + lpar + expr + rpar ).setParseAction( pushFirst ) | ( lpar + expr.suppress() + rpar )).setParseAction(pushUMinus)
+        atom = (Optional("-") + (pi | e | fnumber | ident + lpar + expr + rpar).setParseAction(
+            pushFirst) | (lpar + expr.suppress() + rpar)).setParseAction(pushUMinus)
 
         # by defining exponentiation as "atom [ ^ factor ]..." instead of "atom [ ^ atom ]...", we get right-to-left exponents, instead of left-to-righ
         # that is, 2^3^2 = 2^(3^2), not (2^3)^2.
         factor = Forward()
-        factor << atom + ZeroOrMore( ( expop + factor ).setParseAction( pushFirst ) )
+        factor << atom + ZeroOrMore((expop + factor).setParseAction(pushFirst))
 
-        term = factor + ZeroOrMore( ( multop + factor ).setParseAction( pushFirst ) )
-        expr << term + ZeroOrMore( ( addop + term ).setParseAction( pushFirst ) )
+        term = factor + ZeroOrMore((multop + factor).setParseAction(pushFirst))
+        expr << term + ZeroOrMore((addop + term).setParseAction(pushFirst))
         bnf = expr
     return bnf
 
 # map operator symbols to corresponding arithmetic operations
 epsilon = 1e-12
-opn = { "+" : operator.add,
-        "-" : operator.sub,
-        "*" : operator.mul,
-        "/" : operator.truediv,
-        "^" : operator.pow }
-fn  = { "sin" : math.sin,
-        "cos" : math.cos,
-        "tan" : math.tan,
-        "abs" : abs,
-        "trunc" : lambda a: int(a),
-        "round" : round,
-        "sgn" : lambda a: abs(a)>epsilon and cmp(a,0) or 0}
-def evaluateStack( s ):
+opn = {"+": operator.add,
+       "-": operator.sub,
+       "*": operator.mul,
+       "/": operator.truediv,
+       "^": operator.pow}
+fn = {"sin": math.sin,
+      "cos": math.cos,
+      "tan": math.tan,
+      "abs": abs,
+      "trunc": lambda a: int(a),
+      "round": round,
+      "sgn": lambda a: abs(a) > epsilon and cmp(a, 0) or 0}
+
+
+def evaluateStack(s):
     op = s.pop()
     if op == 'unary -':
-        return -evaluateStack( s )
+        return -evaluateStack(s)
     if op in "+-*/^":
-        op2 = evaluateStack( s )
-        op1 = evaluateStack( s )
-        return opn[op]( op1, op2 )
+        op2 = evaluateStack(s)
+        op1 = evaluateStack(s)
+        return opn[op](op1, op2)
     elif op == "PI":
-        return math.pi # 3.1415926535
+        return math.pi  # 3.1415926535
     elif op == "E":
         return math.e  # 2.718281828
     elif op in fn:
-        return fn[op]( evaluateStack( s ) )
+        return fn[op](evaluateStack(s))
     elif op[0].isalpha():
         return 0
     else:
-        return float( op )
+        return float(op)
